5e5e6c3d425fc3a74882f7980f180e7b8248a000
Unai Gonzalez Sanchez <unaigsanchez@gmail.com>
2023-05-19 21:17:25 +0200

    game setup

---
+++ desktop/src/com/unaigs/snakegame/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.unaigs.snakegame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.unaigs.snakegame.SnakeGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("SnakeGame");
+		new Lwjgl3Application(new SnakeGame(), config);
+	}
+}
